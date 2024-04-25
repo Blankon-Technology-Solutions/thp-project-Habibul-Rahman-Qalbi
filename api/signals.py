0f0ff13aee7134cdc211b50980dca2de99c7af34
Habibul Rahman Qalbi <habibul.rahman@mitech.co.id>
2024-04-25 13:53:08 +0700

    feat: :sparkles: Include Pre_delete signals, remove unused requirements

--- api/signals.py
+++ api/signals.py
@@ -1,13 +1,13 @@
 import json
 
 from api.models import UserTodo
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from django_redis import get_redis_connection
 
 
-@receiver(post_save, sender=UserTodo)
+@receiver([post_save, pre_delete], sender=UserTodo)
 def publish_event(instance: UserTodo, **kwargs):
     event = {
         "message": "New Todo List Updated",
